f11248f6ce8657809dd5a18ba5728429ed90a12c
kamdarkarnavee <kamdarkarnavee@gmail.com>
2019-10-20 19:29:21 -0700

    Initial Commit

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -50,7 +50,7 @@ class StdOutListener(StreamListener):
 if __name__ == '__main__':
     # Authenticate using config.py and connect to Twitter Streaming API.
     hash_tag_list = ["donal trump", "hillary clinton", "barack obama", "bernie sanders"]
-    fetched_tweets_filename = "../json/tweets.json"
+    fetched_tweets_filename = "json/tweets.json"
 
     twitter_streamer = TwitterStreamer()
     twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
