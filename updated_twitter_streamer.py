39b0b5ee6d8cb3127055f1042566a43910d30adf
kamdarkarnavee <kamdarkarnavee@gmail.com>
2019-11-16 20:34:39 -0800

    Update since_id value

--- updated_twitter_streamer.py
+++ updated_twitter_streamer.py
@@ -8,6 +8,6 @@ api = tweepy.API(auth)  # Open/Create a file to append data
 csvFile = open('tweets4.csv', 'a') #Use csv Writer
 csvWriter = csv.writer(csvFile)
 search_term = "bernie sanders"
-for tweet in tweepy.Cursor(api.search, q=search_term, count=100, lang="en", since_id=2014 - 0o6 - 12).items():
+for tweet in tweepy.Cursor(api.search, q=search_term, count=100, lang="en", since_id=2019 - 11 - 16).items():
     print(tweet.created_at, tweet.text)
     csvWriter.writerow([tweet.id, tweet.created_at, tweet.user.screen_name.encode('utf8'), tweet.text.encode('utf8')])
