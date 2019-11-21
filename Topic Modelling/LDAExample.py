ad8f3669d8b47d0d7bfed3c4814df2d5e0dc7f87
Kapil Mulchandani <kapilchandralal.mulchandani@sjsu.edu>
2019-11-21 13:41:19 -0800

    LDA Topic modelling example //Need to change a few things

--- Topic Modelling/LDAExample.py
+++ Topic Modelling/LDAExample.py
@@ -52,6 +52,7 @@ text_data = []
 with open('dataset.csv') as f:
     for line in f:
         tokens = prepare_text_for_lda(line)
+        # Need to change this condition for our Use Case
         number = random.random()
         if number > .99:
             print(tokens)
