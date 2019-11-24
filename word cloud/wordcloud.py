7fd77a5324929248a568e4a574af9978a1011b9a
kamdarkarnavee <kamdarkarnavee@gmail.com>
2019-11-24 02:53:17 -0800

    Generated Word Cloud csv for Tweets by Candidates

--- word cloud/wordcloud.py
+++ word cloud/wordcloud.py
@@ -6,6 +6,7 @@ import string
 from nltk.corpus import stopwords
 # nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
+
 # words that can be considered for removal in pre-processing
 add_stopwords = ['s', 'userhandle', 'user', 'u', 'a', 'amp', 'presidential', '2020', 'democratic']
 
@@ -53,30 +54,34 @@ def preprocess(tweet):
     return tweet
 
 
-# candidates' csv file to read
-filename = ''
-
 # candidate name for word cloud file
-candidate_name = ''
-words_file = candidate_name + 'words.csv'
+candidate_names = ['bernie', 'PeteButtigieg', 'ElizabethWarren', 'JoeBiden', 'KamalaHarris']
 
-data = pd.read_csv(filename, header=None)
-d = [lemma(preprocess(tweet)) for tweet in data[1]] # data[column] where column => column containing tweets
+# Generating word cloud file for each candidate
+for name in candidate_names:
+    # candidates' csv file to read
+    filename = 'E:/255-Project/Political-Opinion-Mining/Candidate Tweets/selected_' + name + '_tweets.csv'
 
-# list of all candidates to be removed from the tweets
-candidates_names = ["bernie", "berni", "sanders", "sander", "joe", "biden", "elizabeth", "warren", "donald", "trump", "mayor", "pete",
-                    "buttigieg", "kamala", "harris", "andrew", "yang", "tulsi", "gabbard"]
-str = ''
-d = str.join(d)
-for i in candidates_names:
-    d = d.replace(i, '')
+    # candidates' csv file to write
+    words_file = name + '_words.csv'
 
-# removal of punctuations
-d = re.sub(r'[^\w\s]', '', d)
+    data = pd.read_csv(filename, header=None, index_col=0)
+    d = [lemma(preprocess(tweet)) for tweet in data[5]] # data[column] where column => column containing tweets
 
-# Convert string to series
-series = pd.Series(d.split())
-print(series)
-series.to_csv(words_file)
+    # list of all candidates to be removed from the tweets
+    candidates_names = ["bernie", "berni", "sanders", "sander", "joe", "biden", "elizabeth", "warren", "donald", "trump", "mayor", "pete",
+                        "buttigieg", "kamala", "harris", "andrew", "yang", "tulsi", "gabbard"]
+    str = ''
+    d = str.join(d)
+    for i in candidates_names:
+        d = d.replace(i, '')
+
+    # removal of punctuations
+    d = re.sub(r'[^\w\s]', '', d)
+
+    # Convert string to series
+    series = pd.Series(d.split())
+    print(series)
+    # series.to_csv(words_file)
 
 
