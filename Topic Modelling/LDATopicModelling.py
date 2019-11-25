3651bfeac4a2cde7518a6e7e228ed09c8826de49
Kapil Mulchandani <kapilchandralal.mulchandani@sjsu.edu>
2019-11-25 04:25:20 -0800

    LDA for our dataset

--- Topic Modelling/LDATopicModelling.py
+++ Topic Modelling/LDATopicModelling.py
@@ -1,26 +1,32 @@
 from nltk.tokenize import RegexpTokenizer
-from stop_words import get_stop_words
 from nltk.stem.porter import PorterStemmer
 from gensim import corpora, models
 import gensim
+import nltk
+from nltk.corpus import stopwords
+import pandas as pd
 
+nltk.download('stopwords')
+stopwords = stopwords.words('english')
+candidates = ['bernie', 'sanders', 'elizabeth', 'warren', 'pete', 'mayor', 'joe', 'biden', 'kamala', 'harris', 'buttigieg']
+stopwords.extend(candidates)
 tokenizer = RegexpTokenizer(r'\w+')
 
+allTweetsdf = pd.read_csv("/home/kapil/PycharmProjects/Political-Opinion-Mining/Candidate_AND_Every_Other_Topic_Query_Tweets/all_candidates_tweets.csv")
 # create English stop words list
-en_stop = get_stop_words('en')
+# en_stop = stopwords
 
 # Create p_stemmer of class PorterStemmer
 p_stemmer = PorterStemmer()
 
-# create sample documents
-doc_a = "Brocolli is good to eat. My brother likes to eat good brocolli, but not my mother."
-doc_b = "My mother spends a lot of time driving my brother around to baseball practice."
-doc_c = "Some health experts suggest that driving may cause increased tension and blood pressure."
-doc_d = "I often feel pressure to perform well at school, but my mother never seems to drive my brother to do better."
-doc_e = "Health professionals say that brocolli is good for your health."
+doc_set = []
+for i in range(0, allTweetsdf.shape[0]):
+    # print(allTweetsdf.iloc[i]['text'])
+
+    doc_set.append(allTweetsdf.iloc[i]['text'])
 
 # compile sample documents into a list
-doc_set = [doc_a, doc_b, doc_c, doc_d, doc_e]
+# doc_set = [doc_a, doc_b, doc_c, doc_d, doc_e]
 
 # list for tokenized documents in loop
 texts = []
@@ -33,7 +39,7 @@ for i in doc_set:
     tokens = tokenizer.tokenize(raw)
 
     # remove stop words from tokens
-    stopped_tokens = [i for i in tokens if not i in en_stop]
+    stopped_tokens = [i for i in tokens if not i in stopwords]
 
     # stem tokens
     stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
@@ -48,7 +54,7 @@ dictionary = corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
 
 # generate LDA model
-ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=2, id2word = dictionary, passes=20)
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=4, id2word = dictionary, passes=20)
 
 print(ldamodel.print_topics(num_topics=2, num_words=4))
 
